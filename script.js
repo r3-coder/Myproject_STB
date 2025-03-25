6aa68aa851c8601e3086d7abca9a3d8428f80723
r3-coder <reys.rodrigues.96@gmail.com>
2025-03-25 03:26:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let imagem = document.querySelector(".imagem-copo")
+let circulo = document.querySelector(".circulo")
+
+function trocaImagem(endereco){
+    imagem.src= endereco
+
+}
+function trocaCor(){
+    circulo.stylebackground = cor
+}
